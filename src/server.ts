7d713666cf357b0c5d50bfdb151a1ec437a2de30
anthonyeunerd <anthony.freitas@eunerd.com>
2023-01-09 21:37:40 -0400

    feat: dotEnv

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,7 @@
 import { app } from "./app";
+import dotenv from 'dotenv'
+dotenv.config();
 
-const PORT = 3333;
+const PORT = process.env.PORT || 3000
 
 app.listen(3333, () => console.log(`runnig in port ${PORT}`));
